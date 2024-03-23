db095bd4618349a5e957fb8c87639dc94a003243
EagleBlood <35221@student.pwsztar.edu.pl>
2024-03-23 14:56:28 +0100

    hall finished

--- styles/styleValues.js
+++ styles/styleValues.js
@@ -4,6 +4,7 @@ export const dimensions = {
     smallTextSize: 12,
     normalTextSize: 16,
     largeTextSize: 20,
+    hugeTextSize: 30,
 
     buttonRadius: 15,
     buttonPadding: 10,
@@ -15,6 +16,19 @@ export const dimensions = {
     seatItemWidth: '10%',
     seatItemHeight: '10%',
     seatItemMargin: '0.5%',
+    inputRadius: 15,
+    inputPadding: 15,
+    inputHigh: 43,
+
+    paddingTopBottom: 40,
+    paddingLeftRight: 20,
+
+    itemGap: 10,
+    itemGapBig: 20,
+    separator: 25,
+    smallSeparator: 15,
+
+    iconSize: 20,
 };
 
 export const colors = {
